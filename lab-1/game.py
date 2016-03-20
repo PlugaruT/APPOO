9b007db0c313a12e64eced5fa5170fec62bc4d16
PlugaruT <plugaru.tudor@gmail.com>
2016-03-20 10:50:07 +0200

    game logic add

--- lab-1/game.py
+++ lab-1/game.py
@@ -7,10 +7,11 @@ WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 CELL_SIZE = 20
 
+FPS = 10
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 DARKGRAY = (50, 50, 50)
-GREEN = (0, 255, 0)
 
 assert WINDOW_WIDTH % CELL_SIZE == 0, "Window width must be a multiple of cell size"
 assert WINDOW_HEIGHT % CELL_SIZE == 0, "Window height must be a multiple of cell size"
@@ -37,7 +38,7 @@ def startingGridRandom(lifeDict):
         lifeDict[item] = random.randint(0,1)
     return lifeDict
 
-#Colours the cells green for life and white for no life
+#Colours the cells black for life and white for no life
 def colourGrid(display, item, lifeDict):
     x = item[0]
     y = item[1]
@@ -46,12 +47,47 @@ def colourGrid(display, item, lifeDict):
     if lifeDict[item] == 0:
         pygame.draw.rect(display, WHITE, (x, y, CELL_SIZE, CELL_SIZE))
     if lifeDict[item] == 1:
-        pygame.draw.rect(display, GREEN, (x, y, CELL_SIZE, CELL_SIZE))
+        pygame.draw.rect(display, BLACK, (x, y, CELL_SIZE, CELL_SIZE))
     return None
 
+#Determines how many alive neighbours there are around each cell
+def getNeighbours(item, lifeDict):
+    neighbours = 0
+    for x in range (-1, 2):
+        for y in range (-1, 2):
+            checkCell = (item[0] + x, item[1] + y)
+            if checkCell[0] < CELL_SIZE  and checkCell[0] >= 0:
+                if checkCell [1] < CELL_SIZE and checkCell[1] >= 0:
+                    if lifeDict[checkCell] == 1:
+                        if x == 0 and y == 0: # negate the central cell
+                            neighbours += 0
+                        else:
+                            neighbours += 1
+    return neighbours
+
+#determines the next generation by running a 'tick'
+def tick(lifeDict):
+    newTick = {}
+    for item in lifeDict:
+        #get number of neighbours for that item
+        numberNeighbours = getNeighbours(item, lifeDict)
+        if lifeDict[item] == 1: # For those cells already alive
+            if numberNeighbours < 2: # kill under-population
+                newTick[item] = 0
+            elif numberNeighbours > 3: #kill over-population
+                newTick[item] = 0
+            else:
+                newTick[item] = 1 # keep status quo (life)
+        elif lifeDict[item] == 0:
+            if numberNeighbours == 3: # cell reproduces
+                newTick[item] = 1
+            else:
+                newTick[item] = 0 # keep status quo (death)
+    return newTick
 
 def main():
     pygame.init()
+    FPSCLOCK = pygame.time.Clock()
     display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Game of Life')
     display.fill(WHITE)
@@ -71,8 +107,17 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+         #runs a tick
+        lifeDict = tick(lifeDict)
+
+        #Colours the live cells, blanks the dead
+        for item in lifeDict:
+            colourGrid(display, item, lifeDict)
+
         drawGrid(display)
         pygame.display.update()
+        FPSCLOCK.tick(FPS)
 
 if __name__ == '__main__':
     main()
