3cf2f26bf9bd17a1d7a4fd689c66925129cdf900
PlugaruT <plugaru.tudor@gmail.com>
2016-03-20 11:00:54 +0200

    game of life patterns add

--- lab-1/game.py
+++ lab-1/game.py
@@ -3,9 +3,9 @@ import sys
 from pygame.locals import *
 import random
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-CELL_SIZE = 20
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 650
+CELL_SIZE = 5
 
 FPS = 10
 
@@ -56,8 +56,8 @@ def getNeighbours(item, lifeDict):
     for x in range (-1, 2):
         for y in range (-1, 2):
             checkCell = (item[0] + x, item[1] + y)
-            if checkCell[0] < CELL_SIZE  and checkCell[0] >= 0:
-                if checkCell [1] < CELL_SIZE and checkCell[1] >= 0:
+            if checkCell[0] < NR_CELL_WIDTH  and checkCell[0] >= 0:
+                if checkCell [1] < NR_CELL_HEIGHT and checkCell[1] >= 0:
                     if lifeDict[checkCell] == 1:
                         if x == 0 and y == 0: # negate the central cell
                             neighbours += 0
@@ -85,6 +85,85 @@ def tick(lifeDict):
                 newTick[item] = 0 # keep status quo (death)
     return newTick
 
+def startingRpentomino(lifeDict):
+    #R-pentomino
+    lifeDict[48,32] = 1
+    lifeDict[49,32] = 1
+    lifeDict[47,33] = 1
+    lifeDict[48,33] = 1
+    lifeDict[48,34] = 1
+    return lifeDict
+
+def startingAcorn(lifeDict):
+    #Acorn
+    lifeDict[105,55] = 1
+    lifeDict[106,55] = 1
+    lifeDict[109,55] = 1
+    lifeDict[110,55] = 1
+    lifeDict[111,55] = 1
+    lifeDict[106,53] = 1
+    lifeDict[108,54] = 1
+    return lifeDict
+
+def startingDiehard(lifeDict):
+    #Diehard
+    lifeDict[45,45] = 1
+    lifeDict[46,45] = 1
+    lifeDict[46,46] = 1
+    lifeDict[50,46] = 1
+    lifeDict[51,46] = 1
+    lifeDict[52,46] = 1
+    lifeDict[51,44] = 1
+    return lifeDict
+
+def startingGosperGliderGun(lifeDict):
+    #Gosper Glider Gun
+    #left square
+    lifeDict[5,15] = 1
+    lifeDict[5,16] = 1
+    lifeDict[6,15] = 1
+    lifeDict[6,16] = 1
+
+    #left part of gun
+    lifeDict[15,15] = 1
+    lifeDict[15,16] = 1
+    lifeDict[15,17] = 1
+    lifeDict[16,14] = 1
+    lifeDict[16,18] = 1
+    lifeDict[17,13] = 1
+    lifeDict[18,13] = 1
+    lifeDict[17,19] = 1
+    lifeDict[18,19] = 1
+    lifeDict[19,16] = 1
+    lifeDict[20,14] = 1
+    lifeDict[20,18] = 1
+    lifeDict[21,15] = 1
+    lifeDict[21,16] = 1
+    lifeDict[21,17] = 1
+    lifeDict[22,16] = 1
+
+    #right part of gun
+    lifeDict[25,13] = 1
+    lifeDict[25,14] = 1
+    lifeDict[25,15] = 1
+    lifeDict[26,13] = 1
+    lifeDict[26,14] = 1
+    lifeDict[26,15] = 1
+    lifeDict[27,12] = 1
+    lifeDict[27,16] = 1
+    lifeDict[29,11] = 1
+    lifeDict[29,12] = 1
+    lifeDict[29,16] = 1
+    lifeDict[29,17] = 1
+
+    #right square
+    lifeDict[39,13] = 1
+    lifeDict[39,14] = 1
+    lifeDict[40,13] = 1
+    lifeDict[40,14] = 1
+
+    return lifeDict
+
 def main():
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -93,7 +172,9 @@ def main():
     display.fill(WHITE)
 
     lifeDict = blankGrid()
-    lifeDict = startingGridRandom(lifeDict) # Assign random life
+    # lifeDict = startingGridRandom(lifeDict) # Assign random life
+    # lifeDict = startingGosperGliderGun(lifeDict)
+    lifeDict = startingDiehard(lifeDict)
 
     #Colours the live cells, blanks the dead
     for item in lifeDict:
